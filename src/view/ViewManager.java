2c6575ecf820708a372493b0e6dff378040955db
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-12-02 02:39:59 +0700

    Lesson 3 Menu Layout

--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -1,36 +1,116 @@
 package view;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.SpaceRunnerButton;
 
 public class ViewManager {
 
-	private static final int HEIGHT = 600;
-	private static final int WIDTH = 800;
+	private static final int HEIGHT = 650;
+	private static final int WIDTH = 1024;
 	
 	private AnchorPane mainPane;
 	private Scene mainScene;
 	private Stage mainStage;
 	
+	private final static int MENU_BUTTON_START_X = 100;
+	private final static int MENU_BUTTON_START_Y = 150;
+	
+	List<SpaceRunnerButton> menuButtons;
+	
 	public ViewManager() {
+		menuButtons = new ArrayList<>();
 		mainPane = new AnchorPane();
 		mainScene = new Scene(mainPane,WIDTH,HEIGHT);
 		mainStage = new Stage();
 		mainStage.setScene(mainScene);
 		createButtons();
+		createBackground();
+		createLogo();
 	}
 	
 	public Stage getMainStage() {
 		return mainStage;
 	}
 	
-	private void createButtons() {
-		SpaceRunnerButton button = new SpaceRunnerButton("click me!");
+	private void addMenuButton(SpaceRunnerButton button) {
+		button.setLayoutX(MENU_BUTTON_START_X);
+		button.setLayoutY(MENU_BUTTON_START_Y + menuButtons.size() * 100);
+		menuButtons.add(button);
 		mainPane.getChildren().add(button);
+	}
+	
+	
+	private void createButtons() {
+		createStartButton();
+		createScoresButton();
+		createHelpButton();
+		createCreditsButton();
+		createExitButton();
+	}
+	
+	private void createStartButton() {
+		SpaceRunnerButton startButton = new SpaceRunnerButton("PlAY");
+		addMenuButton(startButton);
+	}
+	
+	private void createScoresButton() {
+		SpaceRunnerButton scoresButton = new SpaceRunnerButton("SCORES");
+		addMenuButton(scoresButton);
+	}
+	
+	private void createHelpButton() {
+		SpaceRunnerButton helpButton = new SpaceRunnerButton("HELP");
+		addMenuButton(helpButton);
+	}
+	
+	private void createCreditsButton() {
+		SpaceRunnerButton creditsButton = new SpaceRunnerButton("CREDITS");
+		addMenuButton(creditsButton);
+	}
+	
+	private void createExitButton() {
+		SpaceRunnerButton exitButton = new SpaceRunnerButton("EXIT");
+		addMenuButton(exitButton);
+	}
+	
+	
+	private void createBackground() {
+		Image backgroundImage = new Image("view/resources/blue.png", 256,256, false,true);
+		BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
+		mainPane.setBackground(new Background(background));
+	}
+	
+	private void createLogo() {
+		Text logo = new Text("SPACE RUNNER");
+		try {
+			logo.setFont(Font.loadFont(new FileInputStream("src/model/resources/kenvector_future.ttf"), 50));
+		}
+		catch (FileNotFoundException e) {
+			logo.setFont(Font.font("Verdana", 40));
+		}
+		
+		logo.setLayoutX(480);
+		logo.setLayoutY(100);
+		
+		
+		mainPane.getChildren().add(logo);
+		
 	}
 }
